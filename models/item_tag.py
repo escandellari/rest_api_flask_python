22a006daf29d7da21a63e2e6f94b5995816ec3df
escandellari <elisa.scandellari@gmail.com>
2023-05-31 09:33:00 +0100

    Bug fixes

--- models/item_tag.py
+++ models/item_tag.py
@@ -1,8 +1,8 @@
 from db import db
 
 
-class ItemTags(db.Model):
-    __tablename__ = "items_tag"
+class ItemsTags(db.Model):
+    __tablename__ = "items_tags"
 
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
